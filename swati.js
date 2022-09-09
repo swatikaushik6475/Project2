aa0b6dac328ba45e29e6119a8ad5664b799a81d8
swatikaushik6475 <76221287+swatikaushik6475@users.noreply.github.com>
2022-09-09 17:06:27 +0530

    Add files via upload

---
+++ swati.js
@@ -0,0 +1,28 @@
+let c=prompt("Enter your first name");
+let b=prompt("Enter your second name");
+alert("Hi nice to meet you "+c+" "+b);
+let a=1;
+switch (a) {
+ 
+    case 1:
+        a = 'one';
+        break;
+    case 2:
+        a = 'two';
+        break;
+    default:
+        a = 'not found';
+        break;
+}
+console.log(`The value is ${a}`);
+function littleDetail() {
+    const showMessage = document.getElementById('special');
+    showMessage.innerHTML = "See the New Pokémon Scarlet and Pokémon Violet Trailer Get the latest information, including new Pokémon, the Let’s Go! Feature, and multiple story paths (including a new villainous team).";
+    showMessage.style.display = 'block';
+ }
+function displayingMessage() {
+    const showMessage = document.getElementById('Message');
+    showMessage.innerHTML = 'Hi, I am Swati and I welcome you to my small and simple webpage';
+    showMessage.style.display = 'block';
+    
+ }
